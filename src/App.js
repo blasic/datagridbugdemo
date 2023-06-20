42995781efcd7a8367ef12964490947ae9016639
Boris Lašič <boris@nimbis.si>
2023-06-20 14:20:26 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,58 +4,52 @@ import {DataGridPro, GridToolbarContainer, GridToolbarExport} from "@mui/x-data-
 
 
 const rows = [
-  {id: 1, SalesOrder: 123, isExport: true},
-  {id: 2, SalesOrder: 123, isExport: false},
+    {id: 1, SalesOrder: 123, isExport: true},
+    {id: 2, SalesOrder: 123, isExport: false},
 ]
 
 const CustomToolbar = () => {
-
     return (
         <GridToolbarContainer sx={{displayPrint: 'none'}}>
-               <GridToolbarExport printOptions={{}}/>
+            <GridToolbarExport printOptions={{}}/>
         </GridToolbarContainer>
     );
 }
 
 
-
 function App() {
 
-  const exportColor = (param) => {
-      console.log(param)
-      return param.row.isExport ? "yellow100": ""
-  }
-const columns = useMemo(()=> {
-  return [
-    {
-      field: 'SalesOrder',
-      type: "number",
-      cellClassName: exportColor
-    },
-  ]
-},[])
+    const exportColor = (param) => {
+        console.log(param)
+        return param.row.isExport ? "yellow100" : ""
+    }
+    const columns = useMemo(() => {
+        return [
+            {
+                field: 'SalesOrder',
+                type: "number",
+                cellClassName: exportColor
+            },
+        ]
+    }, [])
 
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <DataGridPro
+                //   sx={}
+                slots={{
+                    toolbar: CustomToolbar
+                }}
+                columns={columns}
+                density={"compact"}
 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-
-
-      <DataGridPro
-          //   sx={}
-slots={{
-    toolbar: CustomToolbar
-}}
-          columns={columns}
-          density={"compact"}
-
-          rowCount={rows.length || 0}
-          rows={rows}
-      />
-    </div>
-  );
+                rowCount={rows.length || 0}
+                rows={rows}
+            />
+        </div>
+    );
 }
 
 export default App;
